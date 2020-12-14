8e66169c170495e20841a46c387920509d7ca04e
lipidex <contact@pierpaoloagamennone.com>
2020-12-14 22:24:49 +0100

    Added run of jar, Added parameter file for relationships

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -8,18 +8,26 @@ from datetime import datetime
 ##############################
 ##############################
 
-DEFAULT_NUM_BLOCKS = 1000
+DEFAULT_NUM_NODES_BLOCK = 1000
+RUN_PATH = os.getcwd() + "/"
 PROJECT_PATH = os.path.normpath(os.path.dirname(os.path.realpath(__file__))+"/../")
 TARGET_DIR = PROJECT_PATH + "/target"
 TARGET_JAR = TARGET_DIR + "/graph-query-jar-with-dependencies.jar"
+FILE_RELATIONSHIPS = "soc-pokec-relationships.txt"
 
 ##############################
 ##############################
 
-JAR_BENCH_CMD = "java -jar {}"
+JAR_BENCH_CMD = "java -jar {} {}"
 
 def benchmark(args):
-    jarBenchCMD = JAR_BENCH_CMD.format(TARGET_JAR)
+    relationships_file = RUN_PATH + args.file
+    if not os.path.isfile(relationships_file):
+        print("File of relationships does not exist: "+relationships_file)
+        exit(1)
+
+
+    jarBenchCMD = JAR_BENCH_CMD.format(TARGET_JAR, relationships_file)
     result = subprocess.run(jarBenchCMD,
                             shell=True,
                             cwd=PROJECT_PATH)
@@ -32,17 +40,16 @@ def benchmark(args):
 MVN_BUILD_CMD = "mvn clean compile assembly:single"
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="Get args for the benchmark")
 
     parser.add_argument("-b", "--build", action="store_true",
                         help="If present, build the project")
     parser.add_argument("-d", "--debug", action="store_true",
                         help="If present, print debug messages")
-    parser.add_argument("-g", "--num_node_block", metavar="N", type=int, default=DEFAULT_NUM_BLOCKS,
+    parser.add_argument("-n", "--num_node_block", metavar="N", type=int, default=DEFAULT_NUM_NODES_BLOCK,
                         help="Number of the nodes loaded from the file")
-    parser.add_argument("-f", "--file", metavar="N", type=str, default=DEFAULT_NUM_BLOCKS,
-                        help="Number of the nodes loaded from the file")
+    parser.add_argument("-f", "--file", metavar="N", type=str, default=FILE_RELATIONSHIPS,
+                        help="Path of the file of relationships")
 
     # Parse the input arguments;
     args = parser.parse_args()
