8961159a005925868b10d422175019b907993647
lipidex <contact@pierpaoloagamennone.com>
2020-12-15 11:13:12 +0100

    Added new args, first version benchmark queries

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -2,6 +2,7 @@ import argparse
 from subprocess import run, Popen, PIPE, STDOUT
 import time
 import os
+import random
 from datetime import datetime
 
 ##############################
@@ -20,6 +21,7 @@ ENCODING = 'utf-8'
 
 JAR_BENCH_CMD = "java -jar {} {} {}"
 
+
 def benchmark(args):
     relationships_file = RUN_PATH + args.file
     if not os.path.isfile(relationships_file):
@@ -27,27 +29,56 @@ def benchmark(args):
         exit(1)
 
     print("Run benchmark!")
-    start = time.time_ns()
-    jarBenchCMD = JAR_BENCH_CMD.format(TARGET_JAR, relationships_file, args.num_nodes_block)
-    jar = Popen(jarBenchCMD, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True, cwd=PROJECT_PATH)
+    start_load = time.time_ns()
+    jar_bench_cmd = JAR_BENCH_CMD.format(TARGET_JAR, relationships_file, args.num_nodes_block)
+    jar = Popen(jar_bench_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True, cwd=PROJECT_PATH)
 
     if jar.stdout.readline()[:-1].decode(ENCODING).lower() == "Dataset loaded!".lower():
         print("Dataset loaded!")
     else:
         print("Dataset not loaded!")
         exit(1)
-    end = time.time_ns()
+    end_load = time.time_ns()
 
     if args.debug:
-        print(f"Benchmark total execution time: {(end - start) / 1_000_000_000:.2f} seconds")
+        print(f"Benchmark total loading relationships time: {(end_load - start_load) / 1_000_000_000:.2f} seconds")
 
-    # for ln in jar.stdout:
-    #    print(ln[:-1].decode(ENCODING))
+    num_queries = args.queries
+    start_queries = time.time_ns()
+    start_query = time.time_ns()
+    jar.stdin.write(str.encode(gen_query(args.queries == "r" if random.randint(2, 11) else int(args.queries))+'\n'))
+    for ln in jar.stdout:
+        num_queries -= 1
+        print(ln[:-1].decode(ENCODING))
+        end_query = time.time_ns()
 
+        if args.debug:
+            print(f"Benchmark query execution time: {(end_query - start_query) / 1_000_000_000:.2f} seconds")
 
+        if num_queries > 0:
+            start_query = time.time_ns()
+            jar.stdin.write(str.encode(gen_query(args.queries == "r" if random.randint(2, 11) else int(args.queries))+'\n'))
+        else:
+            break
+
+    end_queries = time.time_ns()
+
+    if args.debug:
+        print(f"Benchmark {args.queries} queries execution time: {(end_queries - start_queries) / 1_000_000_000:.2f} seconds")
 
 ##############################
 ##############################
+
+
+def gen_query(depth):
+    query = "(a)"
+    for i in range(depth - 1):
+        query += "->("+chr(98 + i)+")"
+    return query
+
+##############################
+##############################
+
 
 MVN_BUILD_CMD = "mvn clean compile assembly:single"
 
@@ -62,6 +93,10 @@ if __name__ == "__main__":
                         help="Number of the nodes loaded from the file")
     parser.add_argument("-f", "--file", metavar="N", type=str, default=FILE_RELATIONSHIPS,
                         help="Path of the file of relationships")
+    parser.add_argument("-q", "--queries", metavar="N", type=int, default=100,
+                        help="Number of the queries to run")
+    parser.add_argument("-dp", "--depth", metavar="N", type=str, default="r",
+                        help="Depth of queries between 2 and 11, default is random")
 
     # Parse the input arguments;
     args = parser.parse_args()
